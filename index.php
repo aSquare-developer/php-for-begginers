6c7e84bcd931b8f59882de8f4e353571a463f2a4
aSquare <artur.anissimov@gmail.com>
2023-02-16 22:46:55 +0200

    Arrays Foreach Alternative Syntax

--- index.php
+++ index.php
@@ -3,33 +3,23 @@
 <head>
     <meta charset="UTF-8">
     <title>Demo</title>
-    <style>
-        body {
-            display: grid;
-            place-items: center;
-            height: 100vh;
-            margin: 0;
-            font-family: sans-serif;
-        }
-
-    </style>
 </head>
 <body>
+    <h1>Recommended Books</h1>
 
     <?php
-        $name = "Dark Matter";
-        $read = true;
-
-        if ($read) {
-            $message = "You have read $name";
-        } else {
-            $message = "You have NOT read $name";
-        }
+        $books = [
+            "Do Androids Dream of Electric Sheep",
+            "The Langoliers",
+            "Hail Mary"
+        ];
     ?>
 
-    <h1>
-        <?= $message ?>
-    </h1>
+    <ul>
+        <?php foreach ($books as $book) : ?>
+            <li><?= $book ?></li>
+        <?php endforeach; ?>
+    </ul>
 
 </body>
 </html>
