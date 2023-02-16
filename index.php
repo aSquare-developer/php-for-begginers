409f2de7874f52c4234331b9b053a06b33821ad0
aSquare <artur.anissimov@gmail.com>
2023-02-17 01:11:29 +0200

    Functions and Filters Functions Array Filtering Checking for Equality

--- index.php
+++ index.php
@@ -12,26 +12,44 @@
             [
                 'name' => 'Do Androids Dream of Electric Sheep',
                 'author' => 'Philip K. Dick',
+                'releaseYear' => 1968,
                 'purchaseUrl' => 'https://example.com'
             ],
             [
-                'name' => 'The Langoliers',
+                'name' => 'Project Hail Mary',
                 'author' => 'Andy Weir',
+                'releaseYear' => 2021,
                 'purchaseUrl' => 'https://example.com'
             ],
             [
-                'name' => 'Hail Mary',
-                'author' => 'Philip K. Dick',
+                'name' => 'The Martian',
+                'author' => 'Andy Weir',
+                'releaseYear' => 2011,
                 'purchaseUrl' => 'https://example.com'
             ],
         ];
+
+
+        function filterByAuthor($books, $author) {
+            $filteredBooks = [];
+
+            foreach ($books as $book) {
+                if($book['author'] === $author) {
+                    $filteredBooks[] = $book;
+                }
+            }
+
+            return $filteredBooks;
+
+        }
+
     ?>
 
     <ul>
-        <?php foreach ($books as $book) : ?>
+        <?php foreach (filterByAuthor($books, 'Andy Weir') as $book) : ?>
             <li>
                 <a href="<?= $book['purchaseUrl'] ?>">
-                    <?= $book['name'] ?>
+                    <?= $book['name'] ?> (<?= $book['releaseYear'] ?>) - By <?= $book['author'] ?>
                 </a>
             </li>
         <?php endforeach; ?>
