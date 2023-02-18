a2bf80c567681ba0850e7ecf7fc77f8cadb14d83
aSquare <artur.anissimov@gmail.com>
2023-02-18 17:46:13 +0200

    Extract a PHP Database Class Classes Constructor Functions Database Connections

--- index.php
+++ index.php
@@ -1,17 +1,14 @@
 <?php
 
 require 'functions.php';
+require 'Database.php';
 //require 'router.php';
 
-
-// connect to our MySQL database.
-$dsn = "mysql:host=localhost;port=3306;dbname=php-for-beginners;charset=utf8mb4";
-$pdo = new PDO($dsn, 'root', 'rootroot');
-
-$statement = $pdo->prepare("select * from posts");
-
-$statement->execute();
-
-$posts = $statement->fetchAll(PDO::FETCH_ASSOC);
+$db = new Database();
+$posts = $db->query('select * from posts')->fetchAll(PDO::FETCH_ASSOC);
 
 dd($posts);
+
+
+
+
