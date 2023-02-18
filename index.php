32907c15a2d42a303741060c54fbbabe4416d75a
aSquare <artur.anissimov@gmail.com>
2023-02-18 20:25:31 +0200

    SQL Injection Vulnerabilities Explained SQL Injection Prepared Statements Parameter Binding

--- index.php
+++ index.php
@@ -5,10 +5,12 @@ require 'Database.php';
 //require 'router.php';
 
 $config = require('config.php');
-
 $db = new Database($config['database']);
 
-$posts = $db->query('select * from posts')->fetchAll();
+$id = $_GET['id'];
+$query = "select * from posts where id = :id";
+
+$posts = $db->query($query, ['id' => $id])->fetch();
 
 dd($posts);
 
