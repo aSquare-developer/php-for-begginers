194955fb796d792d1f81951bfcbcfade192d1008
aSquare <artur.anissimov@gmail.com>
2023-04-13 23:59:37 +0300

    Extract a Form Validation Object

--- views/session/create.view.php
+++ views/session/create.view.php
@@ -35,6 +35,9 @@
                     <?php if (isset($errors['email'])) : ?>
                         <p class="text-red-500 text-xs mt-3"><?= $errors['email'] ?></p>
                     <?php endif; ?>
+                    <?php if (isset($errors['password'])) : ?>
+                        <p class="text-red-500 text-xs mt-3"><?= $errors['password'] ?></p>
+                    <?php endif; ?>
                 </form>
             </div>
         </div>
